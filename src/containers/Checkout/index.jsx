d2f4c145b773e5eceb40222158fe31ab232e7c2b
Pablo Pelardas <pablo.pelardas@gmail.com>
2022-04-19 17:53:00 -0300

    [add] react-helmet

--- src/containers/Checkout/index.jsx
+++ src/containers/Checkout/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import AppContext from '../../context/AppContext';
+import { Helmet } from 'react-helmet';
 
 import './Checkout.css';
 
@@ -20,30 +21,35 @@ const Checkout = () => {
   };
 
   return (
-    <div className="Checkout">
-      <div className="Checkout-content">
-        <h3>{!!cart.length ? `Lista de Pedidos:` : `Sin pedidos`}</h3>
-        {cart.map((item, index) => (
-          <div className="Checkout-item" key={`checkout-item:${item.id}`}>
-            <div className="Checkout-element">
-              <h4>{item.title}</h4>
-              <span>${item.price}</span>
+    <>
+      <Helmet>
+        <title>Lista de pedidos - Platzi Conf Merch</title>
+      </Helmet>
+      <div className="Checkout">
+        <div className="Checkout-content">
+          <h3>{!!cart.length ? `Lista de Pedidos:` : `Sin pedidos`}</h3>
+          {cart.map((item, index) => (
+            <div className="Checkout-item" key={`checkout-item:${item.id}`}>
+              <div className="Checkout-element">
+                <h4>{item.title}</h4>
+                <span>${item.price}</span>
+              </div>
+              <button type="button" onClick={() => handleRemove(index)}>
+                <i className="fas fa-trash-alt"></i>
+              </button>
             </div>
-            <button type="button" onClick={() => handleRemove(index)}>
-              <i className="fas fa-trash-alt"></i>
-            </button>
-          </div>
-        ))}
-      </div>
-      {!!cart.length && (
-        <div className="Checkout-sidebar">
-          <h3>{`Precio Total: $ ${handleSumTotal()}`}</h3>
-          <Link to="/checkout/information">
-            <button type="button">Continuar pedido</button>
-          </Link>
+          ))}
         </div>
-      )}
-    </div>
+        {!!cart.length && (
+          <div className="Checkout-sidebar">
+            <h3>{`Precio Total: $ ${handleSumTotal()}`}</h3>
+            <Link to="/checkout/information">
+              <button type="button">Continuar pedido</button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import initialState from '../../initialState';
 import Products from '../../components/Products';
+import { Helmet } from 'react-helmet';
 
 const Home = () => {
   return (
-    <div>
+    <>
+      <Helmet>
+        <title>Platzi Conf Merch - Productos</title>
+        {/* Twitter para poder añadir un formato cuando comparta en twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@pablopelardas" />
+        <meta name="twitter:creator" content="@pablopelardas" />
+        <meta name="twitter:title" content="Platzi Conf Store" />
+        <meta name="twitter:description" content="Platzi Conf Store" />
+        <meta
+          name="twitter:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        />
+        {/* Formato para Facebook con el estandar OG */}
+        <meta property="og:title" content="Platzi Conf Store" />
+        <meta property="og:description" content="Platzi Conf Store" />
+        <meta
+          property="og:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        />
+        <meta
+          property="og:url"
+          content="https://platzi-conf-merch-24438.web.app/"
+        />
+        <meta property="og:site_name" content="Platzi Conf Store" />
+        <meta property="og:locale" content="es_ES" />
+        <meta property="og:type" content="article" />
+        <meta property="fb:app_id" content="874403600625384" />
+      </Helmet>
       <Products products={initialState.products} />
-    </div>
+    </>
   );
 };
 
